3cd9d6dfb559876d08da1ff608b403840cedef3d
patrickp246@gmail.com <patrickp246@gmail.com>
2018-08-08 12:21:45 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,23 @@
 function forLoop(array) {
   for(var i = 0; i < 25; i++) {
-    array.push(`I am ${i} strange loop.`);
+    if(i === 1){
+      array.push(`I am 1 strange loop.`);
+    } else {
+      array.push(`I am ${i} strange loops.`);
+    }
   }
+}
+
+function whileLoop(number){
+  while(number > 0) {
+    number--;
+    console.log(number);
+  }
+}
+
+function doWhileLoop(array){
+  do{
+    
+  }
+  while();
 }
